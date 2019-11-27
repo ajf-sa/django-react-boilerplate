322d06cbce506d087263637d73a8a716a75c16c6
abdulaziz alfuhigi <abajall@gmail.com>
2019-11-28 02:07:43 +0300

    build production

---
+++ src/backend/build/precache-manifest.617a5a5df807957d84d117ed2a5442fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87654a38b568496608e8820f6faba7d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fe1808e573dd52ebe17",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ffa63a706c64223430a1",
+    "url": "/static/js/2.7a610832.chunk.js"
+  },
+  {
+    "revision": "1fe1808e573dd52ebe17",
+    "url": "/static/js/main.3b3b3eb4.chunk.js"
+  },
+  {
+    "revision": "aa5dd861c3df341bfb4f",
+    "url": "/static/js/runtime-main.0fda84ed.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
