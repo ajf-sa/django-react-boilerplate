59b82c187ff66b46810b3dc5723bc111cef9ceaa
abdulaziz alfuhigi <abajall@gmail.com>
2019-11-30 00:24:52 +0300

    add api for register

--- src/backend/users/views.py
+++ src/backend/users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+from .serializers import RegistrationSerializer
+
+
+@api_view(['POST', ])
+def registration_view(request):
+    if request.method == 'POST':
+        serializer = RegistrationSerializer(data=request.data)
+        data = {}
+        if serializer.is_valid():
+            user = serializer.save()
+            data['response'] = 'successfully registered a new user.'
+            data['phone'] = user.phone
+            data['email'] = user.email
+        else:
+            data = serializer.errors
+        return Response(data)
