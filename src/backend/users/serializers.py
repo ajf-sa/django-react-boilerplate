e74e75c7a2a4ab8de4399014b81b1d979f5900ef
abdulaziz alfuhigi <abajall@gmail.com>
2019-11-30 00:25:37 +0300

    serializers for user

---
+++ src/backend/users/serializers.py
@@ -0,0 +1,38 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+User = get_user_model()
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(
+        style={'input_type': 'password'}, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['phone', 'email', 'password', 'password2']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def save(self):
+        user = User(
+            phone=self.validated_data['phone'],
+            email=self.validated_data['email']
+        )
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+
+        if password != password2:
+            raise serializers.ValidationError(
+                {'password': 'Passwords must match.'})
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'phone', 'email', 'groups']
